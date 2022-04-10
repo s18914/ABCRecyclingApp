62c2c6e54816e449318e61fb7209f7ff7edad9b0
s18914 <s18914@pjwstk.edu.pl>
2022-04-10 20:17:54 +0200

    table component

--- abc-recycling-app/src/components/pages/Transports.js
+++ abc-recycling-app/src/components/pages/Transports.js
@@ -1,54 +1,72 @@
 import React from 'react'
 import Button from '../Button'
+import DataTable from 'react-data-table-component'
+
+
+const columns = [
+    {
+      name: 'Adres',
+      selector: row => row.address,
+    },
+    {
+      name: 'Data',
+      selector: row => row.date,
+    },
+    {
+      name: 'Telefon',
+      selector: row => row.telephone,
+    },
+    {
+      name: 'Status',
+      selector: row => row.status,
+    },
+];
+
+const data = [
+    {
+      id: 1,
+      address: 'Jutrzenki 7a/44 Warszawa',
+      date: '01.01.2022',
+      telephone: '515 765 543',
+      status: 'Planowany',
+    },
+    {
+      id: 1,
+      address: 'Jutrzenki 7a/44 Warszawa',
+      date: '01.01.2022',
+      telephone: '515 765 543',
+      status: 'Planowany',
+    },
+]
 
 const Transports = () => {
+  const [pending, setPending] = React.useState(true);
+  const [rows, setRows] = React.useState([]);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      setRows(data);
+      setPending(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <>
-      <div className='main'>
-      <table cellspacing="30" cellpadding="20" RULES="rows">
-            <h2>Lista transportów</h2>
-            <tr class="table-list strong-list">
-                <td><input type="checkbox"/></td>
-                <td><strong>Lp</strong></td>
-                <td><strong>Adres</strong></td>
-                <td><strong>Data</strong></td>
-                <td><strong>Numer telefonu</strong></td>
-                <td><strong>Status</strong></td>
-            </tr>
-            <tr>
-                <td><input type="checkbox"/></td>
-                <td>1</td>
-                <td>Jutrzenki 7a/44 Warszawa</td>
-                <td>01.01.2022</td>
-                <td>515 765 543</td>
-                <td>Planowany</td>
-            </tr>
-            <tr>
-                <td><input type="checkbox"/></td>
-                <td>2</td>
-                <td>Jutrzenki 7a/44 Warszawa</td>
-                <td>01.01.2022</td>
-                <td>515 765 543</td>
-                <td>Planowany</td>
-            </tr>
-            <tr>
-                <td><input type="checkbox"/></td>
-                <td>3</td>
-                <td>Jutrzenki 7a/44 Warszawa</td>
-                <td>01.01.2022</td>
-                <td>515 765 543</td>
-                <td>Planowany</td>
-            </tr>
-        </table>
-        <div className='btn-panel'>
-          <Button color={'green'} text={'Dodaj'}/>
-          <Button color={'red'} text={'Usuń'}/>
-          <Button color={'blue'} text={'Edytuj'}/>
-        </div>
+    <div className='main'>
+      <DataTable
+        title="Lista transportów"
+        columns={columns}
+        data={rows}
+        progressPending={pending}
+        pagination
+      />
+      <div className='btn-panel'>
+        <Button color={'green'} text={'Dodaj'}/>
+        <Button color={'red'} text={'Usuń'}/>
+        <Button color={'blue'} text={'Edytuj'}/>
       </div>
-      
-    </>
-  )
+    </div>
+  );
 }
 
 export default Transports
