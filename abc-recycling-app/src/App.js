136d53c35cacc42a7577153498c39cd997f8c9f8
s18914 <s18914@pjwstk.edu.pl>
2022-05-01 15:14:33 +0200

    add sales page

--- abc-recycling-app/src/App.js
+++ abc-recycling-app/src/App.js
@@ -6,7 +6,7 @@ import Transports from './components/pages/Transports';
 import TransportAdd from './components/pages/TransportAdd';
 import TransportEdit from './components/pages/TransportEdit';
 import TransportDetails from './components/pages/TransportDetails';
-//import Sales from './pages/Sales';
+import Sales from './components/pages/Sales';
 
 const App = () => {
   return (
@@ -20,7 +20,7 @@ const App = () => {
           <Route path='/transports/edit'exact element={<TransportEdit/>} />
           <Route path='/transports/details'exact element={<TransportDetails/>} />
           <Route path='/transportDelete/:id'exact element={<Transports/>} />
-          {/* <Route path='/sales' component={<Sales/>} /> */}
+          <Route path='/sales' component={<Sales/>} />
         </Routes>
       </Router>
     </div>
---
+++ abc-recycling-app/src/components/pages/Sales.js
@@ -0,0 +1,104 @@
+import React, { useEffect } from 'react'
+import DataTable from 'react-data-table-component'
+import { useState } from "react";
+import Axios from "axios";
+import { FaBalanceScale, FaGlasses } from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa'
+import { FaPen } from 'react-icons/fa'
+import {AiOutlinePlusSquare} from 'react-icons/ai'
+
+const Sales = props => {
+
+  const [saleList, setSaleList] = useState([]);
+  const columns =  [
+    {
+      name: 'Id',
+      width: '60px',
+      selector: row => row.sale_id,
+    },
+    {
+      name: 'Kontrahent',
+      width: '180px',
+      selector: row => row.customerName,
+    },
+    {
+        name: 'Cena',
+        selector: row => row.price,
+    },
+    {
+      name: 'Status wysyłki',
+      width: '180px',
+      selector: row => row.transportAddress,
+    },
+    {
+      name: "",
+      button: true,
+      width: '60px',
+      cell: row => (
+        <a href={'/sales/details'}>
+        <FaGlasses
+          style={{color: '#3286DA', cursor: 'pointer', transform: 'scale(1.4)'}}
+        />
+        </a>
+      )
+    },
+    {
+      name: "",
+      button: true,
+      width: '60px',
+      cell: row => (
+        <a href={'/sales/edit'}>
+        <FaPen
+          style={{color: 'grey', cursor: 'pointer', transform: 'scale(1.4)'}}
+        />
+        </a>
+      )
+    },
+    {
+      name: "",
+      button: true,
+      width: '60px',
+      cell: row => (
+        <FaTimes
+            style={{color: '#D83232', cursor: 'pointer', transform: 'scale(1.5)'}}
+            onClick={() => deleteSale(row.sale_id)}
+        />
+      )
+    },
+  ];
+  
+  const deleteSale =  (id) => {
+    Axios.delete(`http://localhost:3001/SaleDelete/${id}`).then((response) => {
+      setSaleList(
+        saleList.filter((row) => {
+          return row.sale_id !== id;
+        })
+      );
+    });
+  };
+
+  useEffect(() => {
+    Axios('http://localhost:3001/sales').then(
+      response => {
+        setSaleList(response.data);
+      }
+    )
+  });
+
+  return (
+    <div className='main'>
+      <DataTable
+        title="Lista sprzedaży"
+        columns={columns}
+        data={saleList}
+      />
+      <div className='btn-panel'>
+        <a href={'/sales/add'}>
+          <AiOutlinePlusSquare style={{color: 'grey', cursor: 'pointer', transform: 'scale(5.2)'}} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Sales
