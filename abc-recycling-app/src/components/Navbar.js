84418bff0f7c1b93fc8eca4632afa4ee8f2bbae3
s18914 <s18914@pjwstk.edu.pl>
2022-08-13 20:56:41 +0200

    fixes

--- abc-recycling-app/src/components/Navbar.js
+++ abc-recycling-app/src/components/Navbar.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Axios from "axios";
 import * as FaIcons from "react-icons/fa";
-
 import {AiOutlineLogin, AiOutlineClose} from "react-icons/ai";
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import '../css/style.css';
 import { IconContext } from 'react-icons';
-import Logo from'../logo.png';
+import Logo from '../logo.png';
 import SubMenu from './SubMenu';
 import { useNavigate } from "react-router-dom";
 
@@ -27,8 +26,7 @@ function Navbar() {
     });
   });
 
-
-    const showSidebar = () => setSidebar(!sidebar);
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <>
@@ -53,23 +51,15 @@ function Navbar() {
               <Link to='#' className='menu-bars'>
                 <AiOutlineClose />
               </Link>
-            <img  src={Logo} alt="abc-recycling"/>
-            </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-              <ul className='nav-menu-items' onClick={showSidebar}>
-                <li className='navbar-toggle'>
-                  <Link to='#' className='menu-bars'>
-                    <AiIcons.AiOutlineClose />
-                  </Link>
-                </li>
-                {SidebarData.map((item, index) => {
-                  return <SubMenu item={item} key={index} />;
-                })}
-              </ul>
-            </nav>
-          </IconContext.Provider>
-        </>
-    );
+            </li>
+            {SidebarData.map((item, index) => {
+              return <SubMenu item={item} key={index} />;
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
 
 export default Navbar
