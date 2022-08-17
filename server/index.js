c810cacf559ab9c90a93db99495b7daa74e47fec
Emilia Cholewicka s18914 <s18914@pjwstk.edu.pl>
2022-08-17 08:26:19 +0200

    add url to .env

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: [process.env.FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
